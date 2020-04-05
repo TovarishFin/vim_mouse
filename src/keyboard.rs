50d752b4546fd9df6649bc924f57215164bd6182
TovarishFin <tovarishfin@gmail.com>
2020-04-05 15:54:57 +0200

    mostly working activate mapping

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,13 +1,49 @@
 use crate::state;
+use state::State;
 use std::mem::MaybeUninit;
 use std::os::raw::c_int;
-use x11::xlib::{
-    self, Display, FocusChangeMask, FocusOut, KeyPress, KeyPressMask, KeyRelease, KeyReleaseMask,
-    MotionNotify, PointerMotionMask, Window, XEvent,
+use x11::{
+    xlib::{
+        self, Display, FocusChangeMask, FocusOut, GrabModeAsync, KeyPress, KeyPressMask,
+        KeyRelease, KeyReleaseMask, MotionNotify, PointerMotionMask, Window, XEvent,
+    },
+    xtest,
 };
+
+pub fn grab_key(display: *mut Display, window: Window, keycode: i32) {
+    unsafe {
+        xlib::XGrabKey(display, keycode, 0, window, 1, GrabModeAsync, GrabModeAsync);
+    }
+}
+
+pub fn ungrab_key(display: *mut Display, window: Window, keycode: i32) {
+    unsafe {
+        xlib::XUngrabKey(display, keycode, 0, window);
+    }
+}
+
+pub fn grab_keyboard(display: *mut Display, window: Window) {
+    unsafe {
+        xlib::XGrabKeyboard(display, window, 1, GrabModeAsync, GrabModeAsync, 0);
+    }
+}
+
+pub fn ungrab_keyboard(display: *mut Display) {
+    unsafe {
+        xlib::XUngrabKeyboard(display, 0);
+    }
+}
+
+pub fn simulate_key(display: *mut Display, keycode: u32, pressed: bool) {
+    unsafe {
+        xtest::XTestFakeKeyEvent(display, keycode, pressed as i32, 0);
+    }
+}
 
 pub fn handle_keyboard_events(display: *mut Display, root_window: Window, log_mouse: bool) {
     unsafe {
+        let mut state = State::new(display, root_window);
+
         let mut focused_window = MaybeUninit::<Window>::uninit();
         let mut revert = MaybeUninit::<c_int>::uninit();
         let mut event = MaybeUninit::<XEvent>::uninit();
@@ -15,8 +51,6 @@ pub fn handle_keyboard_events(display: *mut Display, root_window: Window, log_mo
 
         xlib::XGetInputFocus(display, focused_window.as_mut_ptr(), revert.as_mut_ptr());
         xlib::XSelectInput(display, *focused_window.as_ptr(), mask);
-
-        let mut state = state::State::new(display, root_window);
 
         loop {
             xlib::XNextEvent(display, event.as_mut_ptr());
@@ -31,10 +65,10 @@ pub fn handle_keyboard_events(display: *mut Display, root_window: Window, log_mo
                     xlib::XSelectInput(display, *focused_window.as_ptr(), mask);
                 }
                 KeyPress => {
-                    state.handle_key_press(event.key);
+                    state.handle_key_press(event.key.keycode);
                 }
                 KeyRelease => {
-                    state.handle_key_release(event.key);
+                    state.handle_key_release(event.key.keycode);
                 }
                 MotionNotify if log_mouse => println!("mouse moved"),
                 _ => (),
--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,7 @@
 use crate::cursor;
-use x11::xlib::{Display, Window, XKeyEvent};
+use crate::keyboard;
+use std::time::{Duration, Instant};
+use x11::xlib::{Display, Window};
 
 #[derive(Debug)]
 pub struct State {
@@ -20,7 +22,12 @@ pub struct State {
     left_click_mapping: u32,
     middle_click_mapping: u32,
     right_click_mapping: u32,
+    cached_keys: [Option<u32>; 2],
 
+    sleep_start: Instant,
+    sleep_duration: Duration,
+    active_key_threshold: Duration,
+    active_last_pressed: Instant,
     active: bool,
     fast_pressed: bool,
     slow_pressed: bool,
@@ -36,7 +43,7 @@ pub struct State {
 
 impl State {
     pub fn new(display: *mut Display, window: Window) -> Self {
-        Self {
+        let new_self = Self {
             display,
             window,
             log: false,
@@ -53,6 +60,11 @@ impl State {
             left_click_mapping: 55,   // v
             middle_click_mapping: 56, // b
             right_click_mapping: 57,  // n
+            cached_keys: [None, None],
+            sleep_start: Instant::now(),
+            sleep_duration: Duration::from_millis(10),
+            active_key_threshold: Duration::from_millis(500),
+            active_last_pressed: Instant::now(),
             active: false,
             fast_pressed: false,
             slow_pressed: false,
@@ -64,11 +76,19 @@ impl State {
             left_click_pressed: false,
             middle_click_pressed: false,
             right_click_pressed: false,
-        }
+        };
+
+        keyboard::grab_key(
+            new_self.display,
+            new_self.window,
+            new_self.activate_mapping as i32,
+        );
+
+        new_self
     }
 
-    fn handle_active(&mut self, pressed: bool) {
-        self.active = pressed;
+    pub fn activate_mapping(&self) -> u32 {
+        self.activate_mapping
     }
 
     fn handle_fast(&mut self, pressed: bool) {
@@ -170,12 +190,79 @@ impl State {
         }
     }
 
-    pub fn handle_key_press(&mut self, event: XKeyEvent) {
-        let keycode = event.keycode;
+    fn handle_other(&mut self, keycode: u32, pressed: bool) {
+        if self.active {
+            keyboard::simulate_key(self.display, keycode, pressed);
+        }
+    }
+    fn sleep_activate(&mut self) {
+        self.sleep_start = Instant::now();
+    }
 
-        if self.log && self.log_mouse {
-            let location = cursor::get_cursor_position(self.display, self.window);
-            println!("pre cursor location: {:#?}", location);
+    fn check_activate_sleep(&self) -> bool {
+        if self.sleep_start.elapsed() > self.sleep_duration {
+            return false;
+        }
+
+        return true;
+    }
+
+    fn start_activate_threshold(&mut self) {
+        self.active_last_pressed = Instant::now();
+    }
+
+    fn check_activate_threshold(&self) -> bool {
+        self.active_last_pressed.elapsed() > self.active_key_threshold
+    }
+
+    fn handle_active(&mut self, pressed: bool) {
+        if pressed {
+            keyboard::grab_keyboard(self.display, self.window);
+
+            self.start_activate_threshold();
+        } else {
+            keyboard::ungrab_keyboard(self.display);
+            let activate_was_held = self.check_activate_threshold();
+            if !activate_was_held {
+                keyboard::ungrab_key(self.display, self.window, self.activate_mapping as i32);
+                self.sleep_activate();
+                keyboard::simulate_key(self.display, self.activate_mapping, true);
+                keyboard::simulate_key(self.display, self.activate_mapping, false);
+
+                if let Some(keycode) = self.cached_keys[0].take() {
+                    keyboard::simulate_key(self.display, keycode, false);
+                    keyboard::simulate_key(self.display, keycode, true);
+                    keyboard::simulate_key(self.display, keycode, false);
+
+                    if let Some(keycode) = self.cached_keys[1].take() {
+                        keyboard::simulate_key(self.display, keycode, false);
+                        keyboard::simulate_key(self.display, keycode, true);
+                        keyboard::simulate_key(self.display, keycode, false);
+                    }
+                }
+
+                keyboard::grab_key(self.display, self.window, self.activate_mapping as i32);
+            }
+        }
+
+        self.active = pressed;
+    }
+
+    fn cache_key(&mut self, keycode: u32) {
+        if let None = self.cached_keys[0] {
+            self.cached_keys[0] = Some(keycode);
+        } else {
+            self.cached_keys[1] = Some(keycode);
+        }
+    }
+
+    pub fn handle_key_press(&mut self, keycode: u32) {
+        if keycode != self.activate_mapping {
+            self.cache_key(keycode);
+        }
+
+        if self.check_activate_sleep() {
+            return;
         }
 
         match keycode {
@@ -190,21 +277,14 @@ impl State {
             x if x == self.left_click_mapping => self.handle_left_click(true),
             x if x == self.middle_click_mapping => self.handle_middle_click(true),
             x if x == self.right_click_mapping => self.handle_right_click(true),
-            x => println!("unmapped key pressed: {}", x),
-        }
-
-        if self.log {
-            println!("{:#?}", self);
-        }
-
-        if self.log && self.log_mouse {
-            let location = cursor::get_cursor_position(self.display, self.window);
-            println!("pre cursor location: {:#?}", location);
+            x => self.handle_other(x, true),
         }
     }
 
-    pub fn handle_key_release(&mut self, event: XKeyEvent) {
-        let keycode = event.keycode;
+    pub fn handle_key_release(&mut self, keycode: u32) {
+        if self.check_activate_sleep() {
+            return;
+        }
 
         match keycode {
             x if x == self.activate_mapping => self.handle_active(false),
@@ -218,11 +298,13 @@ impl State {
             x if x == self.left_click_mapping => self.handle_left_click(false),
             x if x == self.middle_click_mapping => self.handle_middle_click(false),
             x if x == self.right_click_mapping => self.handle_right_click(false),
-            x => println!("unmapped key released: {}", x),
+            x => self.handle_other(x, false),
         }
+    }
+}
 
-        if self.log {
-            println!("{:#?}", self);
-        }
+impl Drop for State {
+    fn drop(&mut self) {
+        keyboard::ungrab_key(self.display, self.window, self.activate_mapping as i32);
     }
 }
